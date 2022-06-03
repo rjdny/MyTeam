894b9322521782b094bf127f56131cffe5e9dc11
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2022-06-03 10:36:58 -0500

    console logs teammates birthdays

---
+++ main.js
@@ -0,0 +1,8 @@
+const logBirthdays = () => {
+    console.log('Evan: 5/9/1999')
+    console.log('Tiffany: 10/3/1999')
+    console.log('Seth: 7/19/2002')
+    console.log('Ryan: 1/17/2005')
+}
+
+logBirthdays()
