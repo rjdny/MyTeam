dd1adef3491d6c3dc2df21bd98a435d737c65957
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2022-06-03 11:30:50 -0500

    logged birthdays

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const logBirthdays = () => {
     console.log('Evan: 5/9/1999')
     console.log('Tiffany: 10/3/1999')
@@ -7,14 +6,3 @@ const logBirthdays = () => {
 }
 
 logBirthdays()
-=======
-const logNames = () => {
-    console.log("Evan Mitchell")
-    console.log("Tiffany Smith")
-    console.log("Ryan Downey")
-    console.log("Seth Lewis")
-}
-
-const names = logNames()
-
->>>>>>> 93fe7c602d54377bd54863bbcfaf421173a61875
